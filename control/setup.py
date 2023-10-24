eff9bd0fff325bf70542b197d1c73c2473cfde84
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-10-24 09:10:31 +0100

    Updated setup infrastructure

--- control/setup.py
+++ control/setup.py
@@ -1,20 +1,10 @@
-"""Setup script for odin_workshop python package."""
-
 import sys
-from setuptools import setup, find_packages
+
+from setuptools import setup
+
 import versioneer
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-setup(name='LiveX',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='LiveX',
-      url='https://github.com/stfc-aeg/LiveX',
-      author='Mika Shearwood',
-      author_email='mika.shearwood@stfc.ac.uk',
-      packages=find_packages(),
-      install_requires=required,
-      zip_safe=False,
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
 )
