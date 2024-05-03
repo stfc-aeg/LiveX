7da3024ec3ff0cab3ab73f034a2be09ef73a0604
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2024-05-03 12:53:01 +0100

    removed unused imports

--- control/test/static/livex-react/src/components/Cameras.jsx
+++ control/test/static/livex-react/src/components/Cameras.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
-import { Container, Stack } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-import { WithEndpoint, useAdapterEndpoint, OdinGraph } from 'odin-react';
+import { useAdapterEndpoint } from 'odin-react';
 
 import OrcaCamera from './OrcaCamera';
 
