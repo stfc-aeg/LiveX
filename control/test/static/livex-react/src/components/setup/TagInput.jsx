6790cfd61ff40531a2151d20caaaf53283ee185c
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2024-08-06 09:24:49 +0100

    Generate metadata UI from JSON

---
+++ control/test/static/livex-react/src/components/setup/TagInput.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import { useState, useMemo } from 'react';
+import { MultiSelect } from 'react-multi-select-component';
+import { WithEndpoint } from 'odin-react';
+
+const EndPointButton = WithEndpoint(Button);
+
+function TagInput(props) {
+    const {options} = props;
+    const {metadataEndPoint} = props;
+    const {field} = props;
+
+    console.log("field:", field)
+
+    const selectOptions = options.map(value => ({
+      label: value,
+      value: value,
+      disabled: false
+    }));
+
+    const [selected, setSelected] = useState([]);
+    const [values, setValues] = useState([]);
+
+    const handleOnChange = (selectedOptions) => {
+      setSelected(selectedOptions);
+
+      const selectedValues = selectedOptions.map(option => option.value);
+      setValues(selectedValues);
+    }
+
+    return (
+      <InputGroup>
+        <InputGroup.Text>
+          Experiment Tags
+        </InputGroup.Text>
+        <MultiSelect
+          options={selectOptions}
+          value={selected}
+          onChange={handleOnChange}
+          labelledBy="Select tags"
+          hasSelectAll={false}
+        />
+        <EndPointButton
+          endpoint={metadataEndPoint}
+          value={values}
+          fullpath={"fields/"+field+"/value"}
+          event_type="click"
+          variant="outline-primary"
+        >Send values</EndPointButton>
+      </InputGroup>
+    )
+}
+
+export default TagInput
+
