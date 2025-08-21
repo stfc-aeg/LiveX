f68f8ecb3dd775c960353b4768c9c26a2d014b15
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2025-08-21 13:31:01 +0100

    #47 result goes after index check

--- firmware/livex/src/taskPid.cpp
+++ firmware/livex/src/taskPid.cpp
@@ -32,18 +32,18 @@ void Core0PIDTask(void * pvParameters)
       xSemaphoreTake(gradientAspcMutex, portMAX_DELAY);
       // Read A
       int idx = modbus_server.combineHoldingRegisters(MOD_HEATERTC_A_IDX_HOLD);
-      float result = mcp[idx].readThermocouple();
       if (0<=idx && idx<num_mcp)
       {
+        float result = mcp[idx].readThermocouple();
         PID_A.input = result;
         modbus_server.floatToInputRegisters(MOD_HEATERTC_A_INP, PID_A.input);
       }
 
       // Read B
       idx = modbus_server.combineHoldingRegisters(MOD_HEATERTC_B_IDX_HOLD);
-      result = mcp[idx].readThermocouple();
       if (0<=idx && idx<num_mcp)
       {
+        float result = mcp[idx].readThermocouple();
         PID_B.input = result;
         modbus_server.floatToInputRegisters(MOD_HEATERTC_B_INP, PID_B.input);
       }
@@ -58,7 +58,7 @@ void Core0PIDTask(void * pvParameters)
           idx = modbus_server.combineHoldingRegisters(MOD_EXTRATC_A_IDX_HOLD + (i-2)*2);
           if (0<=idx && idx<num_mcp)
           {
-            result = mcp[idx].readThermocouple();
+            float result = mcp[idx].readThermocouple();
             modbus_server.floatToInputRegisters(MOD_EXTRATC_A_INP+(i*2), result);
           }
         }
