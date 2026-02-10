35e456914f651a95b6709526195106fdac7f4d33
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2026-02-10 15:25:20 +0000

    #53 sequencerpage to avoid app.jsx rerendering

--- control/test/static/livex-vite/src/App.jsx
+++ control/test/static/livex-vite/src/App.jsx
@@ -3,23 +3,19 @@ import './App.css';
 // import 'odin-react/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
 
 import FurnacePage from './components/furnace/FurnacePage';
 import { OdinApp } from 'odin-react';
-import { useAdapterEndpoint } from 'odin-react';
 import Metadata from './components/setup/Metadata';
 import Cameras from './components/cameras/Cameras';
 import Trigger from './components/setup/Trigger';
 import InferencePage from './components/InferencePage.jsx';
 import GraphPage from './components/GraphPage.jsx'
+import SequencerPage from './components/SequencerPage.jsx';
 
 import Motors from './components/motors/Motors.jsx';
-
-import { OdinSequencer } from 'odin-sequencer-react-ui';
 
 // import plotly from 'plotly.js-dist-min';
 
@@ -30,7 +26,6 @@ function App() {
 
   const endpoint_url = import.meta.env.VITE_ENDPOINT_URL;
 
-  const sequencerEndpoint = useAdapterEndpoint('sequencer', endpoint_url, 1000);
 
   return (
     <OdinApp title="LiveX Controls" navLinks={["Metadata and Setup", "Sequencer", "Furnace Control", "Camera Control", "Monitoring", "Inferencing", "Motors"]}>
@@ -47,7 +42,7 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <OdinSequencer endpoint={sequencerEndpoint}/>
+        <SequencerPage endpoint_url={endpoint_url}/>
       </Row>
       <Row>
         <FurnacePage/>
