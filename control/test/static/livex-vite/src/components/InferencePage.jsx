f2db59dae88d8b27a2064ae001d35050cc4dc9f5
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2026-02-10 15:17:57 +0000

    #69 Inferencing page

---
+++ control/test/static/livex-vite/src/components/InferencePage.jsx
@@ -0,0 +1,24 @@
+import Row from 'react-bootstrap/Row';
+import { useAdapterEndpoint } from 'odin-react';
+import InferenceCard from './cameras/InferenceCard';
+
+
+function InferencePage(props) {
+    const { endpoint_url } = props;
+
+    const cameraEndPoint = useAdapterEndpoint('camera', endpoint_url, 1000);
+
+    return (
+      <Row>
+        {Object.keys(cameras).map((key) => (
+          <InferenceCard
+            key={key}
+            endpoint_url={endpoint_url}
+            name={cameraEndPoint.data[key].camera_name}
+          />
+        ))}
+      </Row>
+    )
+}
+
+export default InferencePage;
