3571e7205b5bbf446d7fe7fb141a0e1b6a59a223
Mika Shearwood <mika.shearwood@stfc.ac.uk>
2026-05-08 07:46:07 +0100

    update config js

--- control/test/static/livex-vite/vite.config.js
+++ control/test/static/livex-vite/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  resolve: {
+    dedupe: ["react", "react-dom", "bootstrap", "react-bootstrap", "odin-react"]
+  },
+
+  optimizeDeps: {
+    include: ["bootstrap", "react-bootstrap"]
+  }
+});
